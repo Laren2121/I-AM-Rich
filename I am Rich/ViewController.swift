da33655a487570a7be62f0175d8ae949023393eb
Laren D'Cruz <larenmark@gmail.com>
2024-09-01 01:19:02 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Laren Mark D'Cruz on 2024-09-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
